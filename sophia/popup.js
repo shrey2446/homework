0df52d6ab98f5b7e3493149a786bc9f716733648
Shreya Agarwal <shreyaagarwal@Shreyas-MacBook-Pro.local>
2019-05-02 16:03:05 -0500

    pop up fixed

--- sophia/popup.js
+++ sophia/popup.js
@@ -3,7 +3,7 @@ var id = 0
 function newPopUp(){
   id = id + 1
   var w = 300
-  var h = 500
+  var h = 300
   var l = Math.random()*(screen.width-w)
   var t = Math.random()*(screen.height-h)
   var properties = `width=${w},height=${h},left=${l},top=${t}`
